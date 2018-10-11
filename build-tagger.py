0dc1ea889b4efc428573f0dc5deea22847321da1
Low Jun Chen Marx <marxlow94@gmail.com>
2018-10-11 20:01:30 +0800

    Implement kneser-ney

--- build-tagger.py
+++ build-tagger.py
@@ -17,7 +17,7 @@ def is_invalid_word(word):
 
     return False
 
-def update_nested_dict(first_key, second_key, nested_dict, count_key):
+def update_nested_dict(first_key, second_key, nested_dict, count_key, unigram_prob):
 
     # First occurence of a first key
     if (first_key in nested_dict) == False:
@@ -28,11 +28,18 @@ def update_nested_dict(first_key, second_key, nested_dict, count_key):
     if (second_key in nested_dict[first_key]) == False:
         nested_dict[first_key][second_key] = 1
         nested_dict[first_key][count_key] += 1
+        # Update unigram type count
+        if (second_key in unigram_prob) == False:
+            unigram_prob[second_key] = 1
+        else:
+            unigram_prob[second_key] += 1
         return
 
     # Increase count of first_key, second_key occurence
     nested_dict[first_key][second_key] += 1
     nested_dict[first_key][count_key] += 1
+
+
     return
 
 # Using Add-one
@@ -40,15 +47,22 @@ def add_one_smoothing(nested_dict, count_key, unknown_key, type_count, num_smoot
     # Add one to the probability of all unknowns
     for first_key in nested_dict.keys():
         first_key_count = nested_dict[first_key][count_key]
+        smoothed_count_denominator = first_key_count + (type_count * num_smooth)
         for second_key in nested_dict[first_key].keys():
             second_key_count = nested_dict[first_key][second_key]
             smoothed_count_numerator = second_key_count + num_smooth
-            smoothed_count_denominator = first_key_count + (type_count * num_smooth)
             nested_dict[first_key][second_key] = smoothed_count_numerator / float(smoothed_count_denominator) 
         nested_dict[first_key][count_key] = first_key_count
         nested_dict[first_key][unknown_key] = num_smooth / smoothed_count_denominator
 
     
+def kneser_smoothing(nested_dict, count_key, kneser_amount):
+    for first_key in nested_dict.keys():
+        first_key_count = nested_dict[first_key][count_key] # C(W0)
+        for second_key in nested_dict[first_key].keys():
+            second_key_count = nested_dict[first_key][second_key] - kneser_amount  # number of C(W0W1) - kneser amount
+            nested_dict[first_key][second_key] = second_key_count / float(first_key_count)
+        nested_dict[first_key][count_key] = first_key_count
 
 def transform_count_to_prob(nested_dict, count_key):
     for first_key in nested_dict.keys():
@@ -76,7 +90,9 @@ def train_model(train_file, model_file):
     full_stop = "."
     tag_word = { }
     tag_tag = { }
-    vocab_count = 0
+    tag_unigram_prob = { }
+    word_unigram_prob = { }
+    vocab_count = len(train_text_list)
     count_key = "count"
     unknown_key = "<UNK>"
 
@@ -91,52 +107,82 @@ def train_model(train_file, model_file):
         # Seen a full stop. Update tag to be end of sentence.
         if word == full_stop:
             tag = end_sentence
-            update_nested_dict(prev_tag, tag, tag_tag, count_key)
+            update_nested_dict(prev_tag, tag, tag_tag, count_key, tag_unigram_prob)
             # Set prev_tag back to start of sentence.
             prev_tag = start_sentence
             continue
 
         # Update dictionary.
-        update_nested_dict(prev_tag, tag, tag_tag, count_key)
-        update_nested_dict(tag, word, tag_word, count_key)
-        vocab_count += 1
+        update_nested_dict(prev_tag, tag, tag_tag, count_key, tag_unigram_prob)
+        update_nested_dict(tag, word, tag_word, count_key, word_unigram_prob)
         
         # Before going to the next loop set prev_tag
         prev_tag = tag
 
-    # 4. Create P(tag1|tag2) probabilities with counts
-
-    # Count unique word_types
-    word_type_count = 0
-    for first_key in tag_word.keys():
-        word_type_count += len(list(tag_word[first_key].keys()))
-    # Count unique tag_types
-    tag_type_count = len(list(tag_tag.keys()))
-    print("Tag type count: " + str(tag_type_count))
-    print("Word type count: " + str(word_type_count))
+    # 4a. Create P(tag1|tag2) probabilities with counts
 
     # smoothing = NO_SMOOTHING
     smoothing = ADD_ONE
     # smoothing = KNESER_NEY
+    kneser_amount = 0.1 # Must be [0-1]
 
     if (smoothing == ADD_ONE):
+        # Count unique word_types
+        word_type_count = 0
+        for first_key in tag_word.keys():
+            word_type_count += len(list(tag_word[first_key].keys())) - 1
+        # Count unique tag_types
+        tag_type_count = len(list(tag_tag.keys())) - 1
+        # Perform smoothing
         smoothing_count = 1
         add_one_smoothing(tag_word, count_key, unknown_key, word_type_count, smoothing_count)
         add_one_smoothing(tag_tag, count_key, unknown_key, tag_type_count, smoothing_count)
     elif(smoothing == KNESER_NEY):
         # Implement a back-off to use unigram prob
-        unigram_key = "<UNI>"
-        kneser_ney_tag_tag(tag_tag, count_key, unknown_key, tag_type_count, unigram_key)
-        kneser_ney_tag_word(tag_word, count_key, unknown_key, word_type_count, unigram_key)
-        print("Hello")
+        kneser_smoothing(tag_tag, count_key, kneser_amount)
+        kneser_smoothing(tag_word, count_key, kneser_amount)
     else:
         transform_count_to_prob(tag_tag, count_key)
         transform_count_to_prob(tag_word, count_key)
+
+    # 4b. Calculate unigram probability for Kneser
+    total_num_unique_tag_word_pair = 0
+    total_num_unique_tag_tag_pair = 0
+    tag_word_lamda = {}
+    tag_tag_lamda = {}
+
+    for tag in tag_word.keys():
+        num_unique_tag_word_pair = len(list(tag_word[tag].keys()))
+        num_word = tag_word[tag][count_key]
+        lamda = (kneser_amount * num_unique_tag_word_pair) / float(num_word)
+        tag_word_lamda[tag] = lamda
+        total_num_unique_tag_word_pair += num_unique_tag_word_pair
+    
+    for tag in tag_tag.keys():
+        num_unique_tag_tag_pair = len(list(tag_tag[tag].keys()))
+        num_tag = tag_tag[tag][count_key]
+        lamda = (kneser_amount * num_unique_tag_tag_pair) / float(num_word)
+        tag_tag_lamda[tag] = lamda
+        total_num_unique_tag_tag_pair += num_unique_tag_tag_pair
+
+    for index in tag_unigram_prob.keys():
+        count = tag_unigram_prob[index]
+        probability = count / float(total_num_unique_tag_tag_pair)
+        tag_unigram_prob[index] = probability
+    
+    for index in word_unigram_prob.keys():
+        count = word_unigram_prob[index] 
+        probability = count / float(total_num_unique_tag_word_pair)
+        word_unigram_prob[index] = probability
 
     # 5. Concat tag_word, tag_tag into new dictionary
     tables = {}
     tables["tag_tag"] = tag_tag
     tables["tag_word"] = tag_word
+    tables["tag_unigram_prob"] = tag_unigram_prob
+    tables["tag_tag_lamda"] = tag_tag_lamda
+    tables["word_unigram_prob"] = word_unigram_prob
+    tables["tag_word_lamda"] = tag_word_lamda
 
     # 6. Write dictionary to file
     with open(model_file, "w") as data:
--- run-tagger.py
+++ run-tagger.py
@@ -8,25 +8,34 @@ import numpy as np
 import json
 
 
-def get_tag_tag_prob(dictionary, first_tag, second_tag, count_key, tag_unigram_prob):
+# For seen words, we will boost it's probabilty with kneser-ney.
+# For unseen words, we will use add-one smoothing.
+def get_tag_tag_prob(dictionary, first_tag, second_tag, count_key, tag_unigram_prob, lamda):
     unknown_key = "<UNK>"
+    boost = dictionary[first_tag][unknown_key]
+
+    if (second_tag in lamda) == True:
+        boost = tag_unigram_prob[second_tag] * lamda[first_tag]
+        
     if (second_tag in dictionary[first_tag]) == True:
-        return dictionary[first_tag][second_tag]
+        return dictionary[first_tag][second_tag] + boost
     
-    return dictionary[first_tag][unknown_key]
-    # if second_tag in tag_unigram_prob:
-    #     return tag_unigram_prob[second_tag]
-    # return 0
+    return boost
 
-def get_tag_word_prob(dictionary, tag, word, count_key):
+def get_tag_word_prob(dictionary, tag, word, count_key, word_unigram_prob, lamda):
     unknown_key = "<UNK>"
+    boost = dictionary[tag][unknown_key]
+
+    if (word in word_unigram_prob) == True:
+        boost = word_unigram_prob[word] * lamda[tag]
+        
     if (word in dictionary[tag]) == True:
-        return dictionary[tag][word]
+        return dictionary[tag][word] + boost
 
-    return dictionary[tag][unknown_key]
+    return boost
 
 
-def viberti(word_list, tag_tag, tag_word, tag_unigram_prob):
+def viberti(word_list, tag_tag, tag_word, tag_unigram_prob, word_unigram_prob, tag_tag_lamda, tag_word_lamda):
     count_key = "count"
     start_sentence = "<s>"
     end_sentence = "</s>"
@@ -41,8 +50,8 @@ def viberti(word_list, tag_tag, tag_word, tag_unigram_prob):
     for i in range (num_tags):
         curr_tag = tag_list[i]
         curr_word = word_list[0]
-        tag_tag_prob = get_tag_tag_prob(tag_tag, start_sentence, curr_tag, count_key, tag_unigram_prob) 
-        tag_word_prob = get_tag_word_prob(tag_word, curr_tag, curr_word, count_key)
+        tag_tag_prob = get_tag_tag_prob(tag_tag, start_sentence, curr_tag, count_key, tag_unigram_prob, tag_tag_lamda) 
+        tag_word_prob = get_tag_word_prob(tag_word, curr_tag, curr_word, count_key, word_unigram_prob, tag_word_lamda)
         node_value = tag_tag_prob * tag_word_prob
         viterbi[i, 0] = node_value
         back_pointer[i, 0] = 0
@@ -57,7 +66,7 @@ def viberti(word_list, tag_tag, tag_word, tag_unigram_prob):
             for node_index in range(num_tags):
                 # Get tag-tag prob
                 prev_tag = tag_list[node_index]
-                tag_tag_prob = get_tag_tag_prob(tag_tag, prev_tag, curr_tag, count_key, tag_unigram_prob)
+                tag_tag_prob = get_tag_tag_prob(tag_tag, prev_tag, curr_tag, count_key, tag_unigram_prob, tag_tag_lamda)
                 # Get node value of column before (which is a log value)
                 prev_node_value = viterbi[node_index, word_index-1]
                 new_prob_value = prev_node_value * tag_tag_prob
@@ -65,12 +74,9 @@ def viberti(word_list, tag_tag, tag_word, tag_unigram_prob):
                     max_prob_value = new_prob_value
                     back_pointer[tag_index, word_index] = node_index
 
-            tag_word_prob = get_tag_word_prob(tag_word, curr_tag, curr_word, count_key)
+            tag_word_prob = get_tag_word_prob(tag_word, curr_tag, curr_word, count_key, word_unigram_prob, tag_word_lamda)
             node_value = tag_word_prob * max_prob_value
-            # print("Current word: " + curr_word + " | current tag: " + curr_tag + " | Tag tag prob: " + str(tag_tag_prob) + " | prev node val: " + str(prev_node_value))
             viterbi[tag_index, word_index] = node_value
-            # print("Back pointer here: " + str(back_pointer[tag_index][word_index]))
-            # print("Tag here: " + curr_tag + "  | New node_value" + str(node_value))
             
     # Final iteration node -> (End of sentence) </s>
     back_node_index = 0
@@ -78,7 +84,7 @@ def viberti(word_list, tag_tag, tag_word, tag_unigram_prob):
         max_prob_value = -math.inf
         prev_tag = tag_list[node_index]
         prev_node_value = viterbi[node_index, num_words-1]
-        tag_tag_prob = get_tag_tag_prob(tag_tag, prev_tag, end_sentence, count_key, tag_unigram_prob)
+        tag_tag_prob = get_tag_tag_prob(tag_tag, prev_tag, end_sentence, count_key, tag_unigram_prob, tag_tag_lamda)
         new_prob_value = prev_node_value * tag_tag_prob
 
         if (new_prob_value > max_prob_value):
@@ -116,30 +122,12 @@ def tag_sentence(test_file, model_file, out_file):
     tables = json.loads(str(dictionary_text))
     tag_tag = tables["tag_tag"]
     tag_word = tables["tag_word"]
-
-
-    # Get unigram probabilities
-    tag_unigram_prob = {}
-    word_unigram_prob = {}
-
-    total_vocab_count = 0
-    for primary_tag in tag_tag.keys():
-        total_vocab_count += tag_tag[primary_tag]["count"]
-
-    for primary_tag in tag_tag.keys():
-        tag_count = tag_tag[primary_tag]["count"]
-        unigram_prob = tag_count / total_vocab_count
-        tag_unigram_prob[primary_tag] = unigram_prob
+    tag_unigram_prob = tables["tag_unigram_prob"]
+    tag_tag_lamda = tables["tag_tag_lamda"]
+    word_unigram_prob = tables["word_unigram_prob"]
+    tag_word_lamda = tables["tag_word_lamda"]
 
     # # To check for total prob.
-    # for first_key in tag_word.keys():
-    #     prob_count = 0
-    #     for second_key in tag_word[first_key].keys():
-    #         prob_count += tag_word[first_key][second_key]
-    #     prob_count -= tag_word[first_key]["count"]
-    #     print("Tag: " + first_key + " | total prob: " + str(prob_count))
-    #     break
-        
     # 2. Read test file line by line
     with open(test_file, "r") as ins:
         sentences = []
@@ -150,12 +138,12 @@ def tag_sentence(test_file, model_file, out_file):
     with open(out_file, "w") as data:
         for sentence in sentences:
             word_list = sentence.split()
-            tagged_list = viberti(word_list, tag_tag, tag_word, tag_unigram_prob)
+            tagged_list = viberti(word_list, tag_tag, tag_word, tag_unigram_prob, word_unigram_prob, tag_tag_lamda, tag_word_lamda)
             # Write to file after tagging a sentence
             for tagged_word in tagged_list:
                 data.write(tagged_word + " ")
             data.write("\n")
-            # break
+
     print('Finished...')
 
 if __name__ == "__main__":
